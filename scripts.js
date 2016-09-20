f61b8a926394eafe896d91bc4054328ad0798211
edsonmendieta <emendietaj@gmail.com>
2016-09-20 15:13:03 -0700

    openWeather function now executes once location paragraph is filled.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,50 @@
 // when window loads, sends GET request to IP Geo API
 window.addEventListener('load', ipGeo, false);
 
+var cityName;
+var countryCode;
+
 function ipGeo() {
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://ip-api.com/json');
     xhr.onreadystatechange = function() {
 
+        var weatherParameters;
+
         if(this.readyState == 4 && this.status == 200) {
             console.log(JSON.parse(this.response));
 
             // creates & appends string for #locationP
             var locationText = document.createTextNode(JSON.parse(this.response).city + ',' + ' ' + JSON.parse(this.response).region);
-            console.log(locationText);
             document.getElementById('locationP').appendChild(locationText);
+
+            cityName = JSON.parse(this.response).city;
+            countryCode = JSON.parse(this.response).countryCode;
+
+            weatherParameters = 'hello!';
+            console.log(weatherParameters);
+            console.log(cityName);
+            console.log(countryCode);
+
+            openWeather(); // executes openWeather function last.
         }
     }
     xhr.send();
 }
 
-// when window loads, sends GET request to OpenWeatherMap appendChild
+    function openWeather() {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',' + countryCode + '&appid=659c3dd6c7263dc3cd1bc77834892385');
+        xhr.onreadystatechange  = function() {
+
+            if(this.readyState == 4 && this.status == 200) {
+
+                console.log(JSON.parse(this.response));
+
+                
+            }
+        }
+        xhr.send();
+    };
